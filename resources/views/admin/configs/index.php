9bee3738a36c49acb7e3bd26d0d8f795202ba190
twinh <twinhuang@qq.com>
2019-02-22 14:04:00 +0800

    refactoring: template 使用完整路径

--- resources/views/admin/configs/index.php
+++ resources/views/admin/configs/index.php
@@ -41,7 +41,7 @@
 
 <?= $block->js() ?>
 <script>
-  require(['form', 'plugins/admin/js/data-table', 'template'], function (form) {
+  require(['form', 'plugins/admin/js/data-table', plugins/app/libs/artTemplate/template.min], function (form) {
     $('.js-config-form').loadParams().update(function () {
       $recordTable.reload($(this).serialize(), false);
     });
