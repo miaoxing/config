94bdc25fcf15ef6b7b04041ed695c92802f18fc6
twinh <twinhuang@qq.com>
2020-03-21 18:37:22 +0800

    chore: 增加插件的代码自动完成文档

---
+++ docs/auto-completion.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @property    Miaoxing\Config\Service\Config $config 配置服务
+ */
+class ConfigMixin {
+}
+
+/**
+ * @property    Miaoxing\Config\Service\ConfigModel $configModel 配置模型
+ * @method      \Miaoxing\Config\Service\ConfigModel|\Miaoxing\Config\Service\ConfigModel[] configModel()
+ */
+class ConfigModelMixin {
+}
+
+/**
+ * @mixin ConfigMixin
+ * @mixin ConfigModelMixin
+ */
+class AutoCompletion {
+}
+
+/**
+ * @return AutoCompletion
+ */
+function wei()
+{
+    return new AutoCompletion;
+}
+
+/** @var Miaoxing\Config\Service\Config $config */
+$config = wei()->config;
+
+/** @var Miaoxing\Config\Service\ConfigModel $configModel */
+$config = wei()->configModel();
+
+/** @var Miaoxing\Config\Service\ConfigModel|Miaoxing\Config\Service\ConfigModel[] $configModels */
+$configs = wei()->configModel();
