7b940b81c633c53b48f1b3fb58c718e922acad8e
twinh <twinhuang@qq.com>
2020-08-09 17:12:16 +0800

    chore: 标记 metadata 功能为 internal

--- src/Metadata/ConfigTrait.php
+++ src/Metadata/ConfigTrait.php
@@ -6,15 +6,17 @@ namespace Miaoxing\Config\Metadata;
  * ConfigTrait
  *
  * @property int $id
- * @property string $server
- * @property int $type 值的类型,默认0为字符串
+ * @property bool $type 值的类型,默认0为字符串
  * @property string $name
- * @property string $value
+ * @property mixed $value
  * @property string $comment
  * @property string $createdAt
  * @property string $updatedAt
  * @property int $createdBy
  * @property int $updatedBy
+ * @property string $deletedAt
+ * @property int $deletedBy
+ * @internal will change in the future
  */
 trait ConfigTrait
 {
@@ -24,14 +26,15 @@ trait ConfigTrait
      */
     protected $casts = [
         'id' => 'int',
-        'server' => 'string',
-        'type' => 'int',
+        'type' => 'bool',
         'name' => 'string',
-        'value' => 'string',
+        'value' => 'mixed',
         'comment' => 'string',
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
         'created_by' => 'int',
         'updated_by' => 'int',
+        'deleted_at' => 'datetime',
+        'deleted_by' => 'int',
     ];
 }
