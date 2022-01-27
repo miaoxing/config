a91e61fcef31f8853391f8f161829a90d41c1552
twinh <twinhuang@qq.com>
2022-01-27 23:35:31 +0800

    refactor(config): 移除 Config 相关功能，改用 plugin 插件的 Config 服务
    BREAKING CHANGES: 移除 Config 相关功能，改用 plugin 插件的 Config 服务

--- docs/auto-completion.php
+++ docs/auto-completion.php
@@ -1,23 +1,10 @@
 <?php
 
 /**
- * @property    Miaoxing\Config\Service\Config2 $config2 配置服务
- */
-class Config2Mixin {
-}
 
-/**
- * @property    Miaoxing\Config\Service\ConfigModel $configModel 配置模型
- * @method      Miaoxing\Config\Service\ConfigModel configModel() 返回当前对象
  */
-class ConfigModelMixin {
-}
-
-/**
- * @mixin Config2Mixin
- * @mixin ConfigModelMixin
- */
-class AutoCompletion {
+class AutoCompletion
+{
 }
 
 /**
@@ -25,14 +12,6 @@ class AutoCompletion {
  */
 function wei()
 {
-    return new AutoCompletion;
+    return new AutoCompletion();
 }
 
-/** @var Miaoxing\Config\Service\Config2 $config2 */
-$config2 = wei()->config2;
-
-/** @var Miaoxing\Config\Service\ConfigModel $config */
-$config = wei()->configModel;
-
-/** @var Miaoxing\Config\Service\ConfigModel|Miaoxing\Config\Service\ConfigModel[] $configs */
-$configs = wei()->configModel();
--- src/ConfigPlugin.php
+++ src/ConfigPlugin.php
@@ -10,9 +10,4 @@ class ConfigPlugin extends BasePlugin
      * {@inheritdoc}
      */
     protected $name = '配置';
-
-    public function onAppInit()
-    {
-        //wei()->config->checkUpdate();
-    }
 }
