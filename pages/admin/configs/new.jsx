eae53f6da4c8b7a7aff12a7158c7e371a4f3401b
twinh <twinhuang@qq.com>
2024-07-14 15:47:05 +0800

    feat(u): 调整页面内容为卡片视图

--- pages/admin/configs/new.jsx
+++ pages/admin/configs/new.jsx
@@ -1,10 +1,11 @@
 /**
  * @share [id]/edit
  */
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormItem, FormAction} from '@mxjs/a-form';
-import {Select} from '@miaoxing/admin';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormItem, FormAction } from '@mxjs/a-form';
+import { Select } from '@miaoxing/admin';
+import { Section } from '@mxjs/a-section';
 
 const New = () => {
   return (
@@ -16,15 +17,17 @@ const New = () => {
       <Form>
         {({id}) => {
           return <>
-            <FormItem label="名称" name="name" type={id ? 'plain' : 'text'} required/>
+            <Section>
+              <FormItem label="名称" name="name" type={id ? 'plain' : 'text'} required/>
 
-            <FormItem label="类型" name="type" required>
-              <Select url="consts/configModel-type" labelKey="name" valueKey="id"/>
-            </FormItem>
+              <FormItem label="类型" name="type" required>
+                <Select url="consts/configModel-type" labelKey="name" valueKey="id"/>
+              </FormItem>
 
-            <FormItem label="值" name="value" required/>
+              <FormItem label="值" name="value" required/>
 
-            <FormItem label="注释" name="comment" type="textarea"/>
+              <FormItem label="注释" name="comment" type="textarea"/>
+            </Section>
             <FormAction/>
           </>;
         }}
--- pages/admin/global-configs/new.jsx
+++ pages/admin/global-configs/new.jsx
@@ -1,11 +1,12 @@
 /**
  * @share [id]/edit
  */
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormItem, FormAction} from '@mxjs/a-form';
-import {Select} from '@miaoxing/admin';
-import {Switch} from 'antd';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormItem, FormAction } from '@mxjs/a-form';
+import { Select } from '@miaoxing/admin';
+import { Switch } from 'antd';
+import { Section } from '@mxjs/a-section';
 
 const New = () => {
   return (
@@ -17,19 +18,21 @@ const New = () => {
       <Form>
         {({id}) => {
           return <>
-            <FormItem label="名称" name="name" type={id ? 'plain' : 'text'} required/>
+            <Section>
+              <FormItem label="名称" name="name" type={id ? 'plain' : 'text'} required/>
 
-            <FormItem label="类型" name="type" required>
-              <Select url="consts/globalConfigModel-type" labelKey="name" valueKey="id"/>
-            </FormItem>
+              <FormItem label="类型" name="type" required>
+                <Select url="consts/globalConfigModel-type" labelKey="name" valueKey="id"/>
+              </FormItem>
 
-            <FormItem label="值" name="value" required/>
+              <FormItem label="值" name="value" required/>
 
-            <FormItem label="是否预加载" name="preload" required valuePropName="checked">
-              <Switch/>
-            </FormItem>
+              <FormItem label="是否预加载" name="preload" required valuePropName="checked">
+                <Switch/>
+              </FormItem>
 
-            <FormItem label="注释" name="comment" type="textarea"/>
+              <FormItem label="注释" name="comment" type="textarea"/>
+            </Section>
             <FormAction/>
           </>;
         }}
