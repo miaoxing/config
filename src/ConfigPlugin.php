50413276f44bed8172c693b184708de03153b1dc
twinh <twinhuang@qq.com>
2024-11-28 18:01:10 +0800

    refactor: 将系统功能移到系统菜单中

--- src/ConfigPlugin.php
+++ src/ConfigPlugin.php
@@ -14,13 +14,13 @@ class ConfigPlugin extends BasePlugin
 
     public function onAdminMenuGetMenus(AdminMenu $menu)
     {
-        $setting = $menu->child('setting');
-        $configs = $setting->addChild()->setLabel('配置管理')->setUrl('admin/configs');
+        $system = $menu->child('system');
+        $configs = $system->addChild()->setLabel('配置管理')->setUrl('admin/configs');
         $configs->addChild()->setLabel('添加')->setUrl('admin/configs/new');
         $configs->addChild()->setLabel('编辑')->setUrl('admin/configs/[id]/edit');
         $configs->addChild()->setLabel('删除')->setUrl('admin/configs/[id]/delete');
 
-        $configs = $setting->addChild()->setLabel('全局配置管理')->setUrl('admin/global-configs');
+        $configs = $system->addChild()->setLabel('全局配置管理')->setUrl('admin/global-configs');
         $configs->addChild()->setLabel('添加')->setUrl('admin/global-configs/new');
         $configs->addChild()->setLabel('编辑')->setUrl('admin/global-configs/[id]/edit');
         $configs->addChild()->setLabel('删除')->setUrl('admin/global-configs/[id]/delete');
